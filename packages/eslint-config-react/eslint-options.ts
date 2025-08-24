0917de25e15a8c3b3902177c99e348839e5c9c08
huuyafwww <huuya1234fwww@gmail.com>
2025-08-24 20:02:44 +0900

    add option file

---
+++ packages/eslint-config-react/eslint-options.ts
@@ -0,0 +1,8 @@
+import config from './eslint.config';
+
+export default {
+  concurrency: 'auto',
+  overrideConfig: config, // may include non-cloneable values
+  overrideConfigFile: true,
+  stats: true,
+};
